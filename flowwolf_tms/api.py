c7328def48fbd107df192cdfb6a056454df924c3
Anupam <anupamvns0099@gmail.com>
2022-06-01 19:27:23 +0530

    feat: open api for load

---
+++ flowwolf_tms/api.py
@@ -0,0 +1,11 @@
+import frappe
+from frappe import _
+
+@frappe.whitelist(allow_guest=True)
+def get_load_xml(load):
+    if not frappe.db.exists("Load", load):
+        frappe.throw(_("Load not found."))
+    
+    load_doc = frappe.get_doc("Load", load)
+    xml = load_doc.get_xml()
+    return xml
--- flowwolf_tms/tms/doctype/load/load.py
+++ flowwolf_tms/tms/doctype/load/load.py
@@ -46,8 +46,11 @@ class Load(Document):
 		for field in fields:
 			if field.fieldtype not in ["Table", "Table MultiSelect"]:
 				if field_mapping.get(field.fieldname):
+					value = self.get(field.fieldname) or none
+					if field.fieldtype == "Time" and value:
+						value = frappe.utils.get_time_str(value)
 					
-					xml_dict[field_mapping.get(field.fieldname)]= self.get(field.fieldname) or None
+					xml_dict[field_mapping.get(field.fieldname)]= value
 
 			elif field.fieldtype == "Table":
 				child_meta = frappe.get_meta(field.options)
@@ -58,8 +61,12 @@ class Load(Document):
 						temp_dict = {}
 
 						for c_field in child_fields:
-							if field_mapping[field.fieldname].get(c_field.fieldname):	
-								temp_dict[field_mapping[field.fieldname].get(c_field.fieldname)]= row.get(c_field.fieldname) or None
+							if field_mapping[field.fieldname].get(c_field.fieldname):
+								value = row.get(c_field.fieldname) or None
+								if c_field.fieldtype == "Time" and value:
+									value = frappe.utils.get_time_str(value)
+
+								temp_dict[field_mapping[field.fieldname].get(c_field.fieldname)] = value
 
 						if globals().get(f"{field.fieldname}_hc_values"):
 							temp_dict.update(globals().get(f"{field.fieldname}_hc_values"))
